415f17d8feb52a771de4b418a4aaf274dae46ecd
I Komang Suhendra Eka Putra <manghendra@yahoo.co.id>
2017-08-23 23:33:38 +0800

    Data and Event binding

--- src/app/profile.component.ts
+++ src/app/profile.component.ts
@@ -5,4 +5,11 @@ import { Component } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent  { }
+export class ProfileComponent  { 
+  name: string = 'Komang Suhendra Eka';
+  email: string = 'IKomangSuhendra.EkaPutra@mitrais.com';
+
+  showMyName(): void {
+    alert(this.name);
+  }
+}
