162ba0c310f3f48cac9bf8a52bcd8dbe85a63420
I Komang Suhendra Eka Putra <manghendra@yahoo.co.id>
2017-08-24 07:23:56 +0800

    Add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }  from './app.component';
 import { ProfileComponent } from './profile.component';
 
+const appRoutes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+];
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+    BrowserModule, 
+    RouterModule.forRoot(appRoutes)
+  ],
   declarations: [ 
     AppComponent,
     ProfileComponent
